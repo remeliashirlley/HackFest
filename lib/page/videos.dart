f49324f53856b88d8617b43131e6681935f566ca
yanqi <limyanqi@gmail.com>
2022-05-16 01:06:58 +0800

    update for videos

--- lib/page/videos.dart
+++ lib/page/videos.dart
@@ -21,10 +21,19 @@ Widget _buildGrid() => GridView.count(
     padding: const EdgeInsets.all(4),
     mainAxisSpacing: 4,
     crossAxisSpacing: 4,
-    children: _buildGridTileList(1));
+    children: _buildGridTileList(2));
 
-String vidName1 = "Best out of waste - eco DIYs - Upcycling Ideas & Projects";
-String vidName2 = "5 Genius way to reuse old cd | old cd craft ideas | Best out of waste | Artkala";
+// String vidName1 = "Best out of waste - eco DIYs - Upcycling Ideas & Projects";
+// String vidName2 = "5 Genius way to reuse old cd | old cd craft ideas | Best out of waste | Artkala";
+
+const vidNames = ["Best out of waste - eco DIYs - Upcycling Ideas & Projects", "5 Genius way to reuse old cd | old cd craft ideas | Best out of waste | Artkala"];
 
 List<Container> _buildGridTileList(int count) => List.generate(
-    count, (i) => Container(child: Image.asset('Images/Thumbnails/thumbnail$i.png'), Text('vidName$i')));
+    count, (i) =>
+    Container(
+      child:
+      Column(
+          children: [Image.asset('Images/Thumbnails/thumbnail$i.png'), Text(vidNames[i])]
+)
+)
+);
