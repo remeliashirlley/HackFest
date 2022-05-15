9946cc534a9ec6023edb3c8595ebecee045f0860
Ning Hui <ninghuiker@gmail.com>
2022-05-16 00:14:42 +0800

    Edited Videos Page
    Added in the content of the videos page where several videos will be displayed, showing their thumbnail and the title of the video.

--- lib/page/videos.dart
+++ lib/page/videos.dart
@@ -12,9 +12,19 @@ class _VideoPageState extends State<VideoPage> {
         backgroundColor: Color.fromRGBO(25, 114, 120, 1),
         title: Text('Browse'),
       ),
-    body:
-      Center(
-        child: Text('videos'),
-      )
+    body: _buildGrid()
   );
 }
+
+Widget _buildGrid() => GridView.count(
+    crossAxisCount: 1,
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridTileList(1));
+
+String vidName1 = "Best out of waste - eco DIYs - Upcycling Ideas & Projects";
+String vidName2 = "5 Genius way to reuse old cd | old cd craft ideas | Best out of waste | Artkala";
+
+List<Container> _buildGridTileList(int count) => List.generate(
+    count, (i) => Container(child: Image.asset('Images/Thumbnails/thumbnail$i.png'), Text('vidName$i')));
