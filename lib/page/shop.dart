0034599feb18dab0a6c386d0e5824a8ec790056b
yanqi <limyanqi@gmail.com>
2022-05-15 17:24:49 +0800

    yq: marketplace + mktplace images

--- lib/page/shop.dart
+++ lib/page/shop.dart
@@ -12,9 +12,20 @@ class _ShopPageState extends State<ShopPage> {
       backgroundColor: Color.fromRGBO(25, 114, 120, 1),
       title: Text('Marketplace'),
     ),
-      body:
-      Center(
-        child: Text('shop'),
-      )
+      body: _buildGrid()
+
   );
 }
+
+Widget _buildGrid() => GridView.count(
+    crossAxisCount: 2,
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridTileList(5));
+
+// The images are saved with names pic0.jpg, pic1.jpg...pic29.jpg.
+// The List.generate() constructor allows an easy way to create
+// a list when objects have a predictable naming pattern.
+List<Container> _buildGridTileList(int count) => List.generate(
+    count, (i) => Container(child: Image.asset('Images/Marketplace/pic$i.png')));
